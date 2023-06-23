df7a2705d348f00162fc51d94d82a48c9613a7d8
Yann BLAUDIN DE THE <yann.blaudin-de-the@thalesaleniaspace.com>
2023-06-23 15:23:34 +0200

    Fixing GHA

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 <!-- insertion marker -->
+## [v0.1.2]() - 2023-06-23
+
+<small>[Compare with v0.1.1]()</small>
+
 ## [v0.1.1]() - 2023-06-23
 
 <small>[Compare with v0.1.0]()</small>
--- pdm_build.py
+++ pdm_build.py
@@ -1,17 +1,22 @@
 from setuptools import Extension
 from mypyc.build import mypycify
+from pdm.backend.hooks.base import Context
 
 
-ext_modules = mypycify(
-    [
-        "src/talismans/__init__.py",
-        "src/talismans/compute.py",
-    ]
-)
-ext_modules.extend(
-    [Extension("talismans.hello", ["src/talismans/hellomodule.cpp", "src/talismans/example.cpp"])]
-)
-
-
-def pdm_build_update_setup_kwargs(context, setup_kwargs):
-    setup_kwargs.update(ext_modules=ext_modules)
+def pdm_build_update_setup_kwargs(context: Context, setup_kwargs: dict):
+    if context.target == "wheel":
+        ext_modules = mypycify(
+            [
+                "src/talismans/__init__.py",
+                "src/talismans/compute.py",
+            ]
+        )
+        ext_modules.extend(
+            [
+                Extension(
+                    "talismans.hello",
+                    ["src/talismans/hellomodule.cpp", "src/talismans/example.cpp"],
+                )
+            ]
+        )
+        setup_kwargs.update(ext_modules=ext_modules)
